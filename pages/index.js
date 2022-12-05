5a1575d43b064f1494b3f509ca502705b8a53cd0
Francisco Reynoso <fjreynoso@pima.edu>
2022-12-05 08:05:28 -0700

    Added Netlify CMS

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,26 @@
+import Head from "next/head";
+import Script from "next/script";
 
 export default function Home() {
   return (
     <div >
+      <Head>
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+      </Head>
       <h1>Netlify CMS Installation Demo</h1>
+      <Script>
+        {`
+          if (window.netlifyIdentity) {
+            window.netlifyIdentity.on("init", user => {
+              if (!user) {
+                window.netlifyIdentity.on("login", () => {
+                  document.location.href = "/admin/";
+                });
+              }
+            })
+          }          
+        `}
+      </Script>
     </div>
   )
 }
